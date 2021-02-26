6834b38507bae5ea46492621b1bbd01b98f9e456
repl.it user <replituser@example.com>
2021-02-26 11:12:27 +0000

    Added messages to output

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,7 @@
+#Display message to standard outuput in multiple lines of code
+print("System Failure Imminent!")
+print("System Reboot has been initiated...\n")
+print("...rebooting sensor system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("System Online")
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print("Hello");
+print("EA Sports servers suck");
