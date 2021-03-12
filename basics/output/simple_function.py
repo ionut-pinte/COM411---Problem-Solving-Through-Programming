3499d91d9f6acffb0f041b444b851c996ba38dd5
repl.it user <replituser@example.com>
2021-03-12 10:06:51 +0000

    Simple function

---
+++ basics/output/simple_function.py
@@ -0,0 +1,13 @@
+#What sound did I hear?
+#rumble
+#That was a loud rumble!
+
+def listen():
+  #Ask user about the sound
+  print("What sound did I heard?")
+  sound=input()
+
+  #Display the message
+  print("\n That was a loud ", sound)
+#call the funtion
+listen()  
