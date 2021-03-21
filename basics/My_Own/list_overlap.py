eaa602fcc05b71f15486a32e299e5dead8e154df
ionut Pinte <pinteionutalin@gmail.com>
2021-03-21 01:33:19 +0000

    List Overlap

---
+++ basics/My_Own/list_overlap.py
@@ -0,0 +1,12 @@
+#write a program that returns a list that contains only the elements that are common between the lists
+# (without duplicates).
+a = [12, 3, 5, 8, 90, 11, 44, 66, 8, 9, 34, 56,79,88]
+b = [12, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,79]
+c = []
+for item in a and b:
+    if item in c:
+        pass
+    else:
+        c.append(item)
+c.sort()
+print("These are the numbers found in BOTH a AND b:", c)
