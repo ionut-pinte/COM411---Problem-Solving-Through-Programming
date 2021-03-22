66dd3e2eab0083dbf1156505ace3b0f039fbe08d
ionut Pinte <pinteionutalin@gmail.com>
2021-03-22 13:48:31 +0000

    List Overlap Comprehensions

--- basics/My_Own/list_overlap.py
+++ basics/My_Own/list_overlap.py
@@ -1,6 +1,6 @@
 #write a program that returns a list that contains only the elements that are common between the lists
 # (without duplicates).
-a = [12, 3, 5, 8, 90, 11, 44, 66, 8, 9, 34, 56,79,88]
+a = [12, 12, 3, 5, 8, 90, 11, 44, 66, 8, 9, 34, 56,79,88]
 b = [12, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,79]
 c = []
 for item in a and b:
