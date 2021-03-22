bb848053cc23a6d632244ef5c7f96440a9c539c0
ionut Pinte <pinteionutalin@gmail.com>
2021-03-22 22:46:47 +0000

    PRINTS THE FIRST AND LAST ELEMENTS OF A LIST

---
+++ basics/My_Own/List_End_Solutions.py
@@ -0,0 +1,8 @@
+x = [5, 10, 15, 20, 25]
+#x = random.sample(range(10),5)
+
+y = []
+
+y.append(x[0])
+y.append(x[-1])
+print('This is our list',x, 'and these are the first and last elements of the list: ',y)
